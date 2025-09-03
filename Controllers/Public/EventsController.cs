031980c55dd096fa6a7dd86a3caeee86ce960e58
LPrabodha <lprabodha1998@gmail.com>
2025-09-03 23:27:36 +0530

    Add Checkout and Events controllers with views for ticket purchasing flow

---
+++ Controllers/Public/EventsController.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace online_event_booking_system.Controllers.Public
+{
+    public class EventsController : Controller
+    {
+        [AllowAnonymous]
+        [HttpGet("events")]
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpGet("events/{id}")]
+        public IActionResult Details(string id)
+        {
+            return View();
+        }
+    }
+}
