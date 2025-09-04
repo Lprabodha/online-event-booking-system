7fbd293021e27ac323b75a8cdfa4b5f4c22b3b6e
LPrabodha <lprabodha1998@gmail.com>
2025-09-05 00:41:18 +0530

    Organizer Dashboard design

--- Controllers/Organizer/OrganizerController.cs
+++ Controllers/Organizer/OrganizerController.cs
@@ -18,37 +18,43 @@ namespace online_event_booking_system.Controllers.Organizer
             return View();
         }
 
-        
+        [HttpGet("organizer/reports")]
         public IActionResult Reports()
         {
             return View();
         }
 
+        [HttpGet("organizer/payouts")]
         public IActionResult Payouts()
         {
             return View();
         }
 
+        [HttpGet("organizer/support")]
         public IActionResult Support()
         {
             return View();
         }
 
+        [HttpGet("organizer/settings")]
         public IActionResult Settings()
         {
             return View();
         }
 
+        [HttpGet("organizer/discounts")]
         public IActionResult Discounts()
         {
             return View();
         }
 
+        [HttpGet("organizer/create-event")]
         public IActionResult CreateEvent()
         {
             return View();
         }
 
+        [HttpGet("organizer/edit-event/{id}")]
         public IActionResult EditEvent(int id)
         {
             return View();
