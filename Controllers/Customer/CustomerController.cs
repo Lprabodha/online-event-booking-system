1212ed8b156c0f2d0624e494707737eaa99b5bef
LPrabodha <lprabodha1998@gmail.com>
2025-09-06 00:35:27 +0530

    Customer Dashboard design

--- Controllers/Customer/CustomerController.cs
+++ Controllers/Customer/CustomerController.cs
@@ -6,28 +6,40 @@ namespace online_event_booking_system.Controllers.Customer
     [Authorize(Roles = "Customer")]
     public class CustomerController : Controller
     {
+        [HttpGet("customer")]
         public IActionResult Index()
         {
             return View();
         }
 
+        [HttpGet("customer/events")]
         public IActionResult Events()
         {
             return View();
         }
 
+        [HttpGet("customer/bookings")]
         public IActionResult Bookings()
         {
             return View();
         }
 
+        [HttpGet("customer/profile")]
         public IActionResult Profile()
         {
             return View();
         }
 
+        [HttpGet("customer/support")]
         public IActionResult Support()
         {
+            return View();
+        }
+
+        public IActionResult OrderDetails(int id)
+        {
+            // TODO: Fetch order details from database using the id
+            // For now, return the view with sample data
             return View();
         }
     }
